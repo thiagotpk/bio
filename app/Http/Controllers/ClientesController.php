268806fb38dd0f6db8e924409008acad047c2977
thiagotpk <thiago250694@gmail.com>
2017-10-04 18:03:52 -0300

    final

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -13,7 +13,8 @@ class ClientesController extends Controller
 {
     public function index()
     {
-    	return view('clientes.lista');
+        $clientes= Cliente::get();
+    	return view('clientes.lista', ['clientes'=>$clientes]);
     }
 
     public function novo()
@@ -30,6 +31,36 @@ class ClientesController extends Controller
     	\Session::flash('mensagem_sucesso', 'Cliente cadastrado com sucesso!');
 
     	return Redirect::to('clientes/novo');
+
+    }
+
+    public function editar($id){
+
+        $cliente = Cliente::findOrFail($id);
+
+        return view('clientes.formulario', ['cliente' => $cliente]);
+    }
+
+    public function atualizar($id, Request $request){
+
+        $cliente = Cliente::findOrFail($id);
+
+        $cliente->update($request->all());
+
+        \Session::flash('mensagem_sucesso', 'Cliente atualizado com sucesso!');
+
+        return Redirect::to('clientes/'.$cliente->id.'/editar');
+    }
+
+    public function deletar($id){
+
+        $cliente = Cliente::findOrFail($id);
+
+        $cliente->delete();
+
+        \Session::flash('mensagem_sucesso', 'Cliente deletado com sucesso!');
+
+        return Redirect::to('clientes');
 
     }
 }
